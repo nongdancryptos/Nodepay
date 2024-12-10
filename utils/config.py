369319f1d444f938a5956589c7e42f99c77b0f5b
nongdancryptos <138687332+nongdancryptos@users.noreply.github.com>
2024-12-10 14:34:31 +0700

    Create config.py

---
+++ utils/config.py
@@ -0,0 +1,16 @@
+# utils/config.py
+DOMAIN_API = {
+    "SESSION": "https://api.nodepay.ai/api/auth/session",
+    "PING": [
+        "http://52.77.10.116/api/network/ping",
+        "http://13.215.134.222/api/network/ping",
+        "http://18.136.143.169/api/network/ping",
+        "http://52.74.35.173/api/network/ping",
+        "http://18.142.214.13/api/network/ping",
+        "http://18.142.29.174/api/network/ping",
+        "http://52.74.31.107/api/network/ping"
+    ],
+    "DAILY_CLAIM": "https://api.nodepay.ai/api/mission/complete-mission",
+    "GET_MISSION": "https://api.nodepay.ai/api/mission",
+    "GET_USER_REWARD": "https://api.nodepay.ai/api/mission/user-reward"
+}
